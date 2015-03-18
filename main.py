4aab0217179748984c2a859910b5e26175ad915a
Gangeshwar Krishnamurthy <gangeshwark@gmail.com>
2015-03-18 18:49:24 +0530

    update

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ from flask import Flask, request
 from flask.templating import render_template
 
 app = Flask(__name__)
-from jinja2 import Environment, PackageLoader
-
-env = Environment(loader=PackageLoader('main', 'static/templates'))
-
-
 class datas():
     title = "Hello"
     body = "Hello Tathagat, Neel & Tushar!"
@@ -14,20 +9,16 @@ class datas():
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html", data=datas)
-
+    return render_template('index.html', data=datas)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = 'Hello User!'
+    msg = 'Hello User!'
     if request.form['username'] != "admin":
-        error = "Invalid username!"
+        msg = "Invalid username!"
     elif request.form['password'] != "admin":
-        error = 'Invalid password!'
-
-    tem = env.get_template('verify.html')
-    return tem.render(title=error)
-
+        msg = 'Invalid password!'
+    return render_template('verify.html', title=msg)
 
 if __name__ == '__main__':
     app.run()
