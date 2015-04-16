032e4b0f8ea2e95357d5ff71d2d6c9509e707b25
Gangeshwar Krishnamurthy <gangeshwark@gmail.com>
2015-04-16 17:05:01 +0530

    APIs edit

---
+++ api/HelloWorldAPI.py
@@ -0,0 +1,69 @@
+import endpoints
+from protorpc import remote
+from protorpc import messages
+from protorpc import message_types
+
+package = 'Hello'
+
+
+class Greeting(messages.Message):
+    """Greeting that stores a message."""
+    message = messages.StringField(1)
+
+
+class GreetingCollection(messages.Message):
+    """Collection of Greetings."""
+    items = messages.MessageField(Greeting, 1, repeated=True)
+
+
+STORED_GREETINGS = GreetingCollection(items=[
+    Greeting(message='hello world!'),
+    Greeting(message='goodbye world!'),
+])
+
+__author__ = 'GANGESHWAR'
+
+WEB_CLIENT_ID = 'replace this with your web client application ID'
+ANDROID_CLIENT_ID = 'replace this with your Android client ID'
+IOS_CLIENT_ID = 'replace this with your iOS client ID'
+ANDROID_AUDIENCE = WEB_CLIENT_ID
+
+
+@endpoints.api(name="helloworld", version='v1', allowed_client_ids=[WEB_CLIENT_ID, ANDROID_CLIENT_ID,
+                                                                    IOS_CLIENT_ID, endpoints.API_EXPLORER_CLIENT_ID],
+               audiences=[ANDROID_AUDIENCE],
+               scopes=[endpoints.EMAIL_SCOPE])
+class hello(remote.Service):
+    @endpoints.method(message_types.VoidMessage, GreetingCollection,
+                      path='hellogreeting', http_method='GET',
+                      name='greetings.listGreeting')
+    def greetings_list(self, unused_request):
+        return STORED_GREETINGS
+
+    ID_RESOURCE = endpoints.ResourceContainer(
+        message_types.VoidMessage,
+        id=messages.IntegerField(1, variant=messages.Variant.INT32))
+
+    @endpoints.method(ID_RESOURCE, Greeting,
+                      path='hellogreeting/{id}', http_method='GET',
+                      name='greetings.getGreeting')
+    def greeting_get(self, request):
+        try:
+            return STORED_GREETINGS.items[request.id]
+        except (IndexError, TypeError):
+            raise endpoints.NotFoundException('Greeting %s not found.' %
+                                              (request.id,))
+
+    MULTIPLY_METHOD_RESOURCE = endpoints.ResourceContainer(
+        Greeting,
+        times=messages.IntegerField(2, variant=messages.Variant.INT32,
+                                    required=True))
+
+    @endpoints.method(MULTIPLY_METHOD_RESOURCE, Greeting,
+                      path='hellogreeting/{times}', http_method='POST',
+                      name='greetings.multiply')
+    def greetings_multiply(self, request):
+        return Greeting(message=request.message * request.times)
+
+
+apiserver = endpoints.api_server([hello])
---
+++ api/__init__.py
@@ -0,0 +1,2 @@
+__author__ = 'GANGESHWAR'
+import HelloWorldAPI
